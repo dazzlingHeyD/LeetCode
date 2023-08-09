cad07b48a5c2730819f29fe615de0e61b06d942b
Eunyoung.H <83502660+dazzlingHeyD@users.noreply.github.com>
2023-08-09 23:02:09 +0900

    Time: 899 ms (66.11%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,12 +1,8 @@
-SELECT d.Name as Department,
-e.Name as Employee,
-e.Salary as Salary
-FROM Department d, Employee e
-WHERE(
-    SELECT COUNT(distinct Salary)
-    FROM Employee
-    WHERE Salary > e.Salary AND DepartmentId = d.Id
-) < 3 AND e.DepartmentId = d.Id
-ORDER BY d.Id, e.Salary desc;
-
+SELECT Department.name AS Department, Employee.name as Employee, Employee.salary AS Salary
+FROM (
+        SELECT *, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS n
+        FROM Employee
+) AS Employee
+JOIN Department ON Employee.departmentId = Department.id
+WHERE n <= 3
 
